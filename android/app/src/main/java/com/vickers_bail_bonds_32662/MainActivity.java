c593c348ccb347767717bd92f459b68e71fd1200
Crowdbotics <team@crowdbotics.com>
2022-01-02 22:34:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vickers_bail_bonds_32662/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vickers_bail_bonds_32662;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vickers_bail_bonds_32662";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vickers_bail_bonds_32662",
+  url: "https://vickers_bail_bonds_32662.botics.co",
+  api: "https://vickers_bail_bonds_32662.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
